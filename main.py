067afc738bd542360b20800368996370d7ac4306
Franz Bernack <franz.bernack@cranky.de>
2023-04-26 17:35:59 +0200

    Simplify script

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import re
 
 base = "https://shop.fresch-freising.de"
 courses_url = base + "/courses/5"
-file_path = "courses.md"
 
 # get website contents and parse html
 response = requests.get(courses_url)
@@ -15,11 +14,11 @@ courses_html = categories_html.findChildren("div", id=re.compile('^Course-'))
 
 # create markdown string of courses
 courses_markdown = "# Babyschwimmen Kursübersicht"
-for t in courses_html:
+for c in courses_html:
     # parse information
-    headline = t.find('h5', {"class" : "card-title"}).text
-    text = t.find('p').text
-    url = base + t.find('a' ,href=True)['href']
+    headline = c.find('h5', {"class" : "card-title"}).text
+    text = c.find('p').text
+    url = base + c.find('a' ,href=True)['href']
     
     # Strip whitespaces
     headline_stripped = re.sub(' +', ' ', headline).strip()
@@ -30,16 +29,7 @@ for t in courses_html:
 ## {}
 [{}]({})
 """.format(headline_stripped, text_stripped, url)
-
-# open yaml file for reading
-with open(file_path, "r") as courses_file:
-    saved_courses = courses_file.read()
-    # check if there is an update
-    if saved_courses == courses_markdown:
-        print("no new courses")
-    else:
-        print("new courses")
-        
-# open file for writing
-with open(file_path, "w") as courses_file:
+      
+# write markdown file
+with open("courses.md", "w") as courses_file:
     courses_file.write(courses_markdown)
